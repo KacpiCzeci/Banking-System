9de0bd21db888a6986812cfe47cbdd47769525c3
KacpiCzeci <kapixxx666@wp.pl>
2022-04-09 01:25:09 +0200

    #14 #15 Loan class

--- src/com/company/Loan.java
+++ src/com/company/Loan.java
@@ -1,4 +1,37 @@
 package com.company;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 public class Loan {
+    private Long id;
+    private Double amount;
+    private LocalDateTime startDate;
+    private LocalDateTime closeDate;
+    private InterestRate loanRate;
+
+    public Loan(Long id, Double amt, LocalDateTime cD, InterestRate lR){
+        this.id = id;
+        this.amount = amt;
+        this.startDate = LocalDateTime.now();
+        this.closeDate = cD;
+        this.loanRate = lR;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getCloseDate() {
+        return closeDate;
+    }
+
+    public Double payLoan(){
+        Double time = (double) ChronoUnit.YEARS.between(startDate, closeDate);
+        return amount + loanRate.calculateInterestRate("loan", amount, time);
+    }
 }

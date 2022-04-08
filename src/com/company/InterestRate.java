c65fba79b4ec5f5dd45801581d0d67c25ff1cb45
KacpiCzeci <kapixxx666@wp.pl>
2022-04-08 23:49:59 +0200

    #17 #18 InterestRate class

--- src/com/company/InterestRate.java
+++ src/com/company/InterestRate.java
@@ -1,4 +1,32 @@
 package com.company;
 
 public class InterestRate {
+    private Double accountRate;
+    private Double depositRate;
+    private Double loanRate;
+
+    public Double getAccountRate() {
+        return accountRate;
+    }
+
+    public Double getDepositRate() {
+        return depositRate;
+    }
+
+    public Double getLoanRate() {
+        return loanRate;
+    }
+
+    public Double calculateInterestRate(String nameRate, Double principal, Double time){
+        switch (nameRate){
+            case "account":
+                return principal * accountRate * time;
+            case "deposit":
+                return principal * depositRate * time;
+            case "loan":
+                return  principal * loanRate * time;
+            default:
+                return principal * 1.0 * time;
+        }
+    }
 }

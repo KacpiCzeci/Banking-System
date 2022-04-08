13fa3fdccbf28d7583ad560942c9cb8a15aea49a
KacpiCzeci <kapixxx666@wp.pl>
2022-04-08 23:03:29 +0200

    #29 #30 Card class

--- src/com/company/Card.java
+++ src/com/company/Card.java
@@ -1,4 +1,37 @@
 package com.company;
 
+import java.util.Date;
+
 public class Card {
+    private Account myAccount;
+    private Integer number;
+    private Integer cvc;
+    private Date expirationDate;
+
+    public Card(Account acc, Integer num, Integer cvc, Date expD){
+        this.myAccount = acc;
+        this.number = num;
+        this.cvc = cvc;
+        this.expirationDate = expD;
+    }
+
+    public Account getMyAccount(){
+        return this.myAccount;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public Integer getCvc() {
+        return cvc;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void payByCard(){
+        myAccount.useCardPayment();
+    }
 }

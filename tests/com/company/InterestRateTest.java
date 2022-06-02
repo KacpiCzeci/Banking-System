f8c9d22b45bc14bdfdf4217514beac81d9cf66d9
Maciej Ziobrowski <77328018+Ziob3r@users.noreply.github.com>
2022-06-02 14:34:11 +0200

    InterestRate
    Finish Tests

--- tests/com/company/InterestRateTest.java
+++ tests/com/company/InterestRateTest.java
@@ -1,48 +1,100 @@
 package com.company;
 
+import com.company.BankProduct.BankProduct;
+import com.company.InterestRate.ConstantInterestRate;
+import com.company.InterestRate.CurveInterestRate;
 import com.company.InterestRate.InterestRate;
+import com.company.InterestRate.LinearInterestRate;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class InterestRateTest {
 
-//    private InterestRate interestRate = null;
-//
-//    @BeforeEach
-//    void setUp() {
-//        interestRate = new InterestRate(0.1, 0.15, 0.2);
-//    }
-//
-//    @Test
-//    void checkAccountRate() {
-//
-//        assertEquals(629.84,interestRate.calculateInterestRate("account", 1259.67, 5.0), 0.001);
-//    }
-//
-//    @Test
-//    void checkDepositRate() {
-//        assertEquals(600.00,interestRate.calculateInterestRate("deposit",1000.0,4.0));
-//    }
-//
-//    @Test
-//    void checkLoanRate() {
-//        assertEquals(1407.01,interestRate.calculateInterestRate("loan",2345.01,3.0));
-//    }
-//    @Test
-//    void checkPrincipal(){
-//        assertEquals(-1.0,interestRate.calculateInterestRate("loan",-2345.01,3.0));
-//    }
-//
-//    @Test
-//    void checkDefault(){
-//        assertEquals(5000.00,interestRate.calculateInterestRate("xxx", 5000.00, 5.0), 0.001);
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//        interestRate = null;
-//    }
+    public InterestRate interestRate=null;
+
+    @Mock BankProduct bankProductMOCK;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void CheckIfYouGetCurveInterestRate(){
+        //when
+        interestRate=new CurveInterestRate(bankProductMOCK);
+        //then
+        assertEquals(CurveInterestRate.class,interestRate.getClass());
+    }
+    @Test
+    public void CheckIfYouGetConstantInterestRate(){
+        //when
+        interestRate=new ConstantInterestRate(bankProductMOCK);
+        //then
+        assertEquals(ConstantInterestRate.class,interestRate.getClass());
+    }
+    @Test
+    public void CheckIfYouGetLinearInterestRate(){
+        //when
+        interestRate=new LinearInterestRate(bankProductMOCK);
+        //then
+        assertEquals(LinearInterestRate.class,interestRate.getClass());
+    }
+    @Test
+    public void TestLinearInterestRate(){
+        //given
+        BigDecimal expectedInterestRate = new BigDecimal("1200.00");
+        //when
+        interestRate=new LinearInterestRate(bankProductMOCK);
+        //then
+        assertEquals(expectedInterestRate,interestRate.calculateInterestRate(new BigDecimal("0.05"),new BigDecimal("1000"),new BigDecimal("24")));
+    }
+    @Test
+    public void TestConstantInterestRateLessThenFiveMonth(){
+        //given
+        BigDecimal expectedInterestRate = new BigDecimal("160.00");
+        //when
+        interestRate=new ConstantInterestRate(bankProductMOCK);
+        //then
+        System.out.println(interestRate.calculateInterestRate(new BigDecimal("0.04"),new BigDecimal("1000"),new BigDecimal("4")));
+        assertEquals(expectedInterestRate,interestRate.calculateInterestRate(new BigDecimal("0.04"),new BigDecimal("1000"),new BigDecimal("4")));
+    }
+    @Test
+    public void TestConstantInterestRateMoreThenFiveMonthBUTLessThenTen(){
+        //given
+        BigDecimal expectedInterestRate = new BigDecimal("200.00");
+        //when
+        interestRate=new ConstantInterestRate(bankProductMOCK);
+        //then
+        BigDecimal eightMonth= interestRate.calculateInterestRate(new BigDecimal("0.04"),new BigDecimal("1000"),new BigDecimal("8"));
+        BigDecimal nineMonth= interestRate.calculateInterestRate(new BigDecimal("0.04"),new BigDecimal("1000"),new BigDecimal("8"));
+
+        assertArrayEquals(new Object[]{expectedInterestRate,expectedInterestRate},new Object[]{eightMonth,nineMonth});
+    }
+    @Test
+    public void TestConstantInterestRateLessThenTenMonth(){
+        //given
+        BigDecimal expectedInterestRate = new BigDecimal("960.00");
+        //when
+        interestRate=new ConstantInterestRate(bankProductMOCK);
+        //then
+        assertEquals(expectedInterestRate,interestRate.calculateInterestRate(new BigDecimal("0.04"),new BigDecimal("1000"),new BigDecimal("24")));
+    }
+
+    @Test
+    public void TestCurveInterestRate(){
+        //given
+        BigDecimal expectedInterestRate = new BigDecimal("7200.00");
+        //when
+        interestRate=new CurveInterestRate(bankProductMOCK);
+        //then
+        assertEquals(expectedInterestRate,interestRate.calculateInterestRate(new BigDecimal("0.05"),new BigDecimal("1000"),new BigDecimal("12")));
+    }
 }
